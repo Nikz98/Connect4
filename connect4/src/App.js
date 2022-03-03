b7ddedd6234963e2244fd57509980e41f385efd5
nikz98 <nikeshchotai@gmail.com>
2022-03-03 12:11:10 +0000

    html grid

--- connect4/src/App.js
+++ connect4/src/App.js
@@ -1,18 +1,23 @@
 import Footer from './components/Footer';
 import Gameboard from './components/Gameboard';
 import Header from './components/Header'
-import BigGrid from './components/BigGrid';
 
-function App(board) {
-  return (
-    <div className="container">
-      {console.log(board)}
-      <Header/>
-      <Gameboard/>
-      <BigGrid/>
-      <Footer/>
-    </div>
-  );
+function App() {
+
+	let board = [['-', '-', '-', '-', '-', '-', '-'],
+			['-', '-', '-', '-', '-', '-', '-'],
+			['-', '-', '-', '-', '-', '-', '-'],
+			['-', '-', '-', '-', '-', '-', '-'],
+			['-', '-', '-', '-', '-', '-', '-'],
+			['-', '-', '-', '-', '-', '-', '-'],]
+
+	return (
+		<div className="container">
+			<Header />
+			<Gameboard board={board}/>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- connect4/src/components/Gameboard.js
+++ connect4/src/components/Gameboard.js
@@ -1,14 +1,68 @@
-import Slot from "./Slot"
-
-const passID = (id) => {
-    console.log(`You clicked: ${id}`)
-}
- 
 const Gameboard = (board) => {
+
+
+
+    function getCell(board) {
+        if 
+    }
+
     return (
-        <>
-            <Slot onTurn={passID}/>
-        </>
+        <div id="grid">
+        <div className="row" id="row-0">
+            <div className="column" row="0" column="0"></div>
+            <div className="column" row="0" column="1"></div>
+            <div className="column" row="0" column="2"></div>
+            <div className="column" row="0" column="3"></div>
+            <div className="column" row="0" column="4"></div>
+            <div className="column" row="0" column="5"></div>
+            <div className="column" row="0" column="6"></div>
+        </div>
+        <div className="row" id="row-1">
+            <div className="column" row="1" column="0"></div>
+            <div className="column" row="1" column="1"></div>
+            <div className="column" row="1" column="2"></div>
+            <div className="column" row="1" column="3"></div>
+            <div className="column" row="1" column="4"></div>
+            <div className="column" row="1" column="5"></div>
+            <div className="column" row="1" column="6"></div>
+        </div>
+        <div className="row" id="row-2">
+            <div className="column" row="2" column="0"></div>
+            <div className="column" row="2" column="1"></div>
+            <div className="column" row="2" column="2"></div>
+            <div className="column" row="2" column="3"></div>
+            <div className="column" row="2" column="4"></div>
+            <div className="column" row="2" column="5"></div>
+            <div className="column" row="2" column="6"></div>
+        </div>
+        <div className="row" id="row-3">
+            <div className="column" row="3" column="0"></div>
+            <div className="column" row="3" column="1"></div>
+            <div className="column" row="3" column="2"></div>
+            <div className="column" row="3" column="3"></div>
+            <div className="column" row="3" column="4"></div>
+            <div className="column" row="3" column="5"></div>
+            <div className="column" row="3" column="6"></div>
+        </div>
+        <div className="row" id="row-4">
+            <div className="column" row="4" column="0"></div>
+            <div className="column" row="4" column="1"></div>
+            <div className="column" row="4" column="2"></div>
+            <div className="column" row="4" column="3"></div>
+            <div className="column" row="4" column="4"></div>
+            <div className="column" row="4" column="5"></div>
+            <div className="column" row="4" column="6"></div>
+        </div>
+        <div className="row" id="row-5">
+            <div className="column" row="5" column="0"></div>
+            <div className="column" row="5" column="1"></div>
+            <div className="column" row="5" column="2"></div>
+            <div className="column" row="5" column="3"></div>
+            <div className="column" row="5" column="4"></div>
+            <div className="column" row="5" column="5"></div>
+            <div className="column" row="5" column="6"></div>
+        </div>
+    </div>
     )
 }
 
